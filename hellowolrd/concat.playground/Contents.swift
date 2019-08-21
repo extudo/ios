2269b1190f795f39e7b8b6ae9d3e9e68bfdfa7e8
velrino <denismagalhaesvelrino@gmail.com>
2019-08-21 14:50:36 -0300

    Examples concat

---
+++ hellowolrd/concat.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//Data example
+var first = "Lorem"
+var second = "Ipsum"
+var third = "Velrino"
+var fourty = String(1)
+
+//Result example
+var firstExample = first + second + third;
+
+var render = [
+    firstExample,
+    first + second + third,
+    "\(first) \(second) \(third)",
+]
+
---
+++ hellowolrd/concat.swift
@@ -0,0 +1,8 @@
+//
+//  concat.swift
+//  
+//
+//  Created by denis magalhaes on 21/08/19.
+//
+
+import Foundation
